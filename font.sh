9fbc9d642db8246d95b9ecdec89f28aec7db6d07
Technical Helper Chetan Bhalothia <chetan.bhalothia@gmail.com>
2018-02-11 14:58:21 +0530

    Create font.sh

---
+++ font.sh
@@ -0,0 +1,14 @@
+apt-cache search ttf-mscorefonts-installer ttf-mscorefonts-installer - Installer for Microsoft TrueType core fonts
+apt install ttf-mscorefonts-installer
+
+apt install ttf-liberation
+apt install fonts-liberation
+apt install ttf-uralic
+apt install fonts-uralic
+apt install ttf-root-installer
+apt install ttf-freefont
+apt install ttf-dustin
+apt install ttf-linux-libertine
+apt install fonts-linuxlibertine
+apt install fonts-dustin
+apt install ttf-staypuft
